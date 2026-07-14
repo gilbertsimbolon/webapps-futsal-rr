1ad1f2bc0369236c76e460b347b7de38f66edc05
gilbertsimbolon <gilbertttsimbolon@gmail.com>
2026-07-14 19:32:48 +0800

    add: membuat fungsi store pada register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,5 +15,32 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    
+    // fungsi registrasi, (store)
+    public function store(Request $request)
+    {
+        // validasi data
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ], [
+            'name.required' => 'Kolom nama wajib diisi.',
+            'email.required' => 'Kolom email wajib diisi.',
+            'email.email' => 'Email yang Anda masukkan tidak berbentuk email.',
+            'email.unique' => 'Email yang Anda masukkan sudah terdaftar.',
+            'password.required' => 'Password wajib diisi.',
+            'password.min' => 'Password setidaknya memiliki 8 karakter.',
+            'password.confirmed' => 'Konfirmasi password tidak sesuai.',
+        ]);
+
+        // jika validasi data berhasil
+        User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        // kembalikan ke halaman login
+        return redirect()->route('login')->with('success', 'Anda berhasil melakukan registrasi akun.');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,8 +9,7 @@ Route::get('/', function () {
 
 Route::get('/login', function () {
     return view('auth.login');
-});
+})->name('login');
 
-Route::get('/register', function () {
-    return view('auth.register');
-});
+Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
