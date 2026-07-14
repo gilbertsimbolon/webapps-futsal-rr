ce781b92d90f0fae986e0f5120db70c3c8bd392b
gilbertsimbolon <gilbertttsimbolon@gmail.com>
2026-07-14 19:17:21 +0800

    add: membuat halaman registrasi

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('title', 'Registrasi | bkngftsl')
+
+@php
+    $hideLayout = true;
+@endphp
+
+@section('content')
+    <div class="min-vh-100 container d-flex justify-content-center align-items-center">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register Card -->
+                <div class="card px-sm-6 px-0">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="index.html" class="app-brand-link gap-2">
+                                <img src="{{ asset('img/logo.png') }}" alt="Logo UNIMA" style="width: 40px; height: 40px;">
+                                <span class="app-brand-text demo text-heading fw-bold">bkngftsl</span>
+                            </a>
+                        </div>
+                        <!-- /Logo -->
+                        <h4 class="mb-1 mt-3 text-center">Selamat Datang!</h4>
+                        <p class="mb-6 text-center">Silahkan registrasi akun terlebih dahulu.</p>
+
+                        <form id="formAuthentication" class="mb-6" action="index.html">
+                            <!-- Kolom Input Nama -->
+                            <div class="mb-6">
+                                <label for="nama" class="form-label">Nama Lengkap</label>
+                                <input type="text" class="form-control" id="username" name="nama"
+                                    placeholder="Roy Saputra Nainggolan" autofocus />
+                            </div>
+
+                            <!-- Kolom Input Email -->
+                            <div class="mb-6">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="text" class="form-control" id="email" name="email"
+                                    placeholder="roysaputra@gmail.com" />
+                            </div>
+
+                            <!-- Kolom Input Password -->
+                            <div class="mb-6 form-password-toggle">
+                                <label class="form-label" for="password">Password</label>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" />
+                                    <span class="input-group-text cursor-pointer"><i
+                                            class="icon-base bx bx-hide"></i></span>
+                                </div>
+                            </div>
+
+                            <!-- Konfirmasi Password -->
+                            <div class="mb-6 form-password-toggle">
+                                <label class="form-label" for="password">Konfirmasi Password</label>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" />
+                                    <span class="input-group-text cursor-pointer"><i
+                                            class="icon-base bx bx-hide"></i></span>
+                                </div>
+                            </div>
+
+
+                            <button class="btn btn-primary d-grid w-100">Daftar</button>
+                        </form>
+
+                        <p class="text-center">
+                            <span>Sudah memiliki akun?</span>
+                            <a href="auth-login-basic.html">
+                                <span>Login</span>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <!-- Register Card -->
+            </div>
+        </div>
+    </div>
+@endsection
