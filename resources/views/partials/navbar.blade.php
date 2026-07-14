34e26108b0602aac0d982635db32fa4fd49e0b2b
gilbertsimbolon <gilbertttsimbolon@gmail.com>
2026-07-14 13:55:01 +0800

    fiX: memperbaiki navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,61 @@
+<nav class="layout-navbar container-xxl navbar-detached navbar navbar-expand-xl align-items-center bg-navbar-theme"
+    id="layout-navbar">
+    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-4 me-xl-0 d-xl-none">
+        <a class="nav-item nav-link px-0 me-xl-6" href="javascript:void(0)">
+            <i class="icon-base bx bx-menu icon-md"></i>
+        </a>
+    </div>
+
+    <div class="navbar-nav-right d-flex align-items-center justify-content-end" id="navbar-collapse">
+
+        <ul class="navbar-nav flex-row align-items-center ms-md-auto">
+            <!-- User -->
+            <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                <a class="nav-link dropdown-toggle hide-arrow p-0" href="#" data-bs-toggle="dropdown">
+                    <div class="text-end">
+                        <h6 class="mb-0 fw-bold">
+                            {{ auth()->user()->name }}
+                        </h6>
+
+                        <small class="text-muted text-capitalize">
+                            {{ auth()->user()->getRoleNames()->first() }}
+                        </small>
+                    </div>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <div class="d-flex">
+                                <div class="flex-grow-1">
+                                    <h6 class="mb-0 text-capitalize">{{ auth()->user()->name }}</h6>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="dropdown-divider my-1"></div>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <i class="icon-base bx bx-user icon-md me-3"></i><span>My Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="dropdown-divider my-1"></div>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" method="POST" href="#"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
+                            <i class="icon-base bx bx-power-off icon-md me-3"></i><span>Log Out</span>
+                        </a>
+
+                        <form id="logout-form" action="#" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
+            </li>
+            <!--/ User -->
+        </ul>
+    </div>
+</nav>
