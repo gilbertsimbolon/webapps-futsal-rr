dab39d13228e3ccf345fda27b37e7394db41247a
gilbertsimbolon <gilbertttsimbolon@gmail.com>
2026-07-19 23:34:28 +0800

    add: membuat fungsi update data pada controller

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfilController extends Controller
 {
@@ -11,4 +12,23 @@ class ProfilController extends Controller
     {
         return view('profile.index');
     }
+
+    // fungsi update data
+    public function update(Request $request)
+    {
+        // validasi data
+        $request->validate([
+            'name' => 'nullable|string|max:255',
+            'email' => 'nullable|string|email|max:255|unique:users,email,' . Auth::id(),
+        ]);
+
+        // update data user
+        $user = Auth::user();
+        $user->name = $request->name ?? $user->name;
+        $user->email = $request->filled('email') ? $request->email : $user->email;
+
+        $user->save();
+
+        return redirect()->back()->with('success', 'Profil berhasil diperbarui.');
+    }   
 }
