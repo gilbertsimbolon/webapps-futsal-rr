807c289a6480c14651e528eb68ce9c6002932ace
gilbertsimbolon <gilbertttsimbolon@gmail.com>
2026-07-19 23:49:37 +0800

    fix: memperbaiki route update dan ganti password profil

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,8 @@ Route::prefix('profil')->group(function () {
     // Halaman Profil
     Route::get('/', [ProfilController::class, 'index'])->name('profil.index');
     // Edit Data Profil
-    Route::post('/', [ProfilController::class, 'update'])->name('profil.update');
-    Route::post('/', [ProfilController::class, 'updatePassword'])->name('profil.update.password');
+    Route::post('/update', [ProfilController::class, 'update'])->name('profil.update');
+    Route::post('/password', [ProfilController::class, 'updatePassword'])->name('profil.update.password');
 });
 
 // Route Dashboard
