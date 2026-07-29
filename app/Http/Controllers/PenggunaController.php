fa532dea537aae2131d75797a76bfecc109e1ba6
gilbertsimbolon <gilbertttsimbolon@gmail.com>
2026-07-29 14:01:40 +0800

    add: menghubungkan controller dengan halaman manajemen pengguna

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PenggunaController extends Controller
@@ -9,6 +10,8 @@ class PenggunaController extends Controller
     // fungsi index
     public function index()
     {
-        return view('pengguna.index');
+        $pengguna = User::all();
+
+        return view('pengguna.index', compact('pengguna'));
     }
 }
--- resources/views/pengguna/index.blade.php
+++ resources/views/pengguna/index.blade.php
@@ -6,53 +6,56 @@
     <div class="card">
         <h5 class="card-header">Manajemen Pengguna</h5>
         <div class="table-responsive text-nowrap">
-            <table class="table table-hover">
-                <thead>
-                    <tr class="text-center">
-                        <th style="width: 10px" class="text-center">No</th>
-                        <th>Nama</th>
-                        <th>Email</th>
-                        <th>Status Akun</th>
-                        <th style="width: 20px" class="text-center">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody class="table-border-bottom-0">
-                    <tr>
-                        <!-- Nomor -->
-                        <td class="text-center" style="width: 10px">
-                            1
-                        </td>
+            @foreach ($pengguna as $p)
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th style="width: 10px" class="text-center">No</th>
+                            <th>Nama</th>
+                            <th>Email</th>
+                            <th class="text-center">Status Akun</th>
+                            <th style="width: 20px" class="text-center">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                        <tr>
+                            <!-- Nomor -->
+                            <td class="text-center" style="width: 10px">
+                                {{ $loop->iteration }}
+                            </td>
 
-                        <!-- Nama Pengguna -->
-                        <td>Albert Cook</td>
+                            <!-- Nama Pengguna -->
+                            <td>{{ $p->name }}</td>
 
-                        <!-- Email -->
-                        <td>
-                            albertcook@gmail.com
-                        </td>
+                            <!-- Email -->
+                            <td>
+                                {{ $p->email }}
+                            </td>
 
-                        <!-- Status Akun -->
-                        <td class="text-center">
-                            <span class="badge bg-label-primary me-1">Active</span>
-                        </td>
+                            <!-- Status Akun -->
+                            <td class="text-center">
+                                <span class="badge bg-label-primary me-1">Active</span>
+                            </td>
 
-                        <!-- Aksi -->
-                        <td class="text-center">
-                            <div class="dropdown">
-                                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                                    <i class="icon-base bx bx-dots-vertical-rounded"></i>
-                                </button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="javascript:void(0);"><i
-                                            class="icon-base bx bx-edit-alt me-1"></i> Edit</a>
-                                    <a class="dropdown-item" href="javascript:void(0);"><i
-                                            class="icon-base bx bx-trash me-1"></i> Delete</a>
+                            <!-- Aksi -->
+                            <td class="text-center">
+                                <div class="dropdown">
+                                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
+                                        data-bs-toggle="dropdown">
+                                        <i class="icon-base bx bx-dots-vertical-rounded"></i>
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="javascript:void(0);"><i
+                                                class="icon-base bx bx-edit-alt me-1"></i> Edit</a>
+                                        <a class="dropdown-item" href="javascript:void(0);"><i
+                                                class="icon-base bx bx-trash me-1"></i> Delete</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            @endforeach
         </div>
     </div>
 @endsection
